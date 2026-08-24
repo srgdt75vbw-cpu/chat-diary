6b74b5fccf084510d37470edbf5fdfe071f96a0c
김주경 <rkdfur0203@naver.com>
2026-08-24 17:36:22 +0900

    Initial Commit

---
+++ Chat Diary/Chat_DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_DiaryApp.swift
+//  Chat Diary
+//
+//  Created by 김주경 on 8/24/26.
+//
+
+import SwiftUI
+
+@main
+struct Chat_DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chat Diary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chat Diary
+//
+//  Created by 김주경 on 8/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
